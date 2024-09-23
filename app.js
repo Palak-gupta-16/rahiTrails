f920059508f4ee393fd4893f355af53a0918353c
Palak-Gupta-Coder <palakgupta681@gmail.com>
2024-09-23 16:05:35 +0530

    adding express-session & adding public directory for javascripts & stylesheets

--- app.js
+++ app.js
@@ -2,13 +2,10 @@ const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
 const ejsMate = require('ejs-mate');
-const Joi = require('joi');
-const {campgroundSchema , reviewSchema} = require('./schemas.js')
-const catchAsync = require('./utils/catchAsync');
+const session = require('express-session')
 const ExpressError = require('./utils/ExpressError');
 const methodOverride = require('method-override');
-const Campground = require('./models/campground');
-const Review = require('./models/review');
+
 
 const campgrounds = require('./routes/campgrounds');
 const reviews = require('./routes/reviews')
@@ -30,6 +27,20 @@ app.set('views' , path.join(__dirname,'views'));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
+app.use(express.static(path.join(__dirname,'public')));
+
+
+const sessionConfig ={
+    secret: 'thisshouldbeabettersecret!',
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+        httpOnly: true,
+        expires: Date.now() + 1000*60 * 60 * 24 * 7,
+        maxAge: 1000*60 * 60 * 24 * 7
+    }
+}
+app.use(session(sessionConfig))
 
 
 const validateReview = (req,res,next)=>{
