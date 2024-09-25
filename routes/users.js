d81a6463d641c7d2b66e14f502ddd38825fed598
Palak-Gupta-Coder <palakgupta681@gmail.com>
2024-09-25 10:29:16 +0530

    Refactor routes to controllers and restructure route

--- routes/users.js
+++ routes/users.js
@@ -1,50 +1,19 @@
 const express = require('express');
 const router = express.Router();
+const users = require('../controllers/users')
 const catchAsync = require('../utils/catchAsync')
 const User = require('../models/user');
 const passport = require('passport');
 const { storeReturnTo } = require('../middleware');
 
-router.get('/register', (req,res)=>{
-    res.render('users/register');
-})
+router.route('/register')
+    .get( users.renderRegister)
+    .post(catchAsync( users.register));
 
-router.post('/register',catchAsync( async(req,res,next)=>{
-    try{
-    const {email , username, password} = req.body;
-    const user = new User({email,username});
-    const registerUser = await User.register(user, password);
-    req.login(registerUser,err =>{
-        if(err) return next(err);
-        req.flash('success','welcome to Rahitrail!');
-        res.redirect('/campgrounds');
-    })
-    // console.log(registerUser);
-    }catch(e){
-        req.flash('error', e.message);
-        res.redirect('register');
-    }
-}))
+router.route('/login')
+    .get(users.renderLogin)
+    .post(storeReturnTo,passport.authenticate('local', {failureFlash: true,failureRedirect: '/login'}),users.login)
 
-router.get('/login',(req,res)=>{
-    res.render('users/login');
-})
-
-router.post('/login', storeReturnTo,passport.authenticate('local', {failureFlash: true,failureRedirect: '/login'}),(req,res)=>{
-    req.flash('success','welcome back');
-    const redirectUrl = res.locals.returnTo || '/campgrounds'
-    // delete req.session.returnTo;
-    res.redirect(redirectUrl);
-})
-
-router.get('/logout', (req, res, next) => {
-    req.logout(function (err) {
-        if (err) {
-            return next(err);
-        }
-        req.flash('success', 'Goodbye!');
-        res.redirect('/campgrounds');
-    });
-}); 
+router.get('/logout', users.logout); 
 
 module.exports = router;
