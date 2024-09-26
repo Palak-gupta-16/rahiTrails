d1bfab6e760c984d9d35f10a5c10db20a52c5860
Palak-Gupta-Coder <palakgupta681@gmail.com>
2024-09-26 09:14:10 +0530

    Configure Multer, Cloudinary, and dotenv; upload images to Cloudinary and display in a carousel

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -3,11 +3,16 @@ const router = express.Router();
 const campgrounds = require('../controllers/campgrounds')
 const catchAsync = require('../utils/catchAsync');
 const {isLoggedIn, validateCampground,isAuthor} = require('../middleware')
+const multer = require('multer');
+const {storage} = require('../cloudinary')
+const upload = multer({storage})
+
+
 const Campground = require('../models/campground');
 
 router.route('/')
     .get(catchAsync(campgrounds.index))
-    .post(isLoggedIn, validateCampground,catchAsync(campgrounds.createCampground));
+    .post(isLoggedIn,upload.array('image'), validateCampground,catchAsync(campgrounds.createCampground));
 
 router.get('/new',isLoggedIn, catchAsync(campgrounds.renderNewForm));
 
