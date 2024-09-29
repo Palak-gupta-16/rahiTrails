ec2480513b825ca2551431d53d09f921a5c04414
Palak Gupta <palakgupta681@gmail.com>
2024-09-29 11:25:12 +0530

    Set up MongoDB Atlas and configure session store

--- app.js
+++ app.js
@@ -20,6 +20,9 @@ const mongoSanitize = require('express-mongo-sanitize');
 const userRoutes = require('./routes/users')
 const campgroundRoutes = require('./routes/campgrounds');
 const reviewRoutes = require('./routes/reviews');
+
+const MongoStore = require('connect-mongo');
+
 const dbUrl = process.env.DB_URL;
 // 'mongodb://localhost:27017/rahi-trail'
 mongoose.connect(dbUrl);
@@ -44,8 +47,20 @@ app.use(mongoSanitize({
     replaceWith: '_'
 }));
 
+const store = MongoStore.create({
+    mongoUrl: dbUrl,
+    touchAfter: 24 * 60 * 60,
+    crypto: {
+        secret: 'thisshouldbeabettersecret!'
+    }
+});
+
+store.on("error",function(e){
+    console.log("session error",e);
+})
 
 const sessionConfig ={
+    store,
     name: 'session',
     secret: 'thisshouldbeabettersecret!',
     resave: false,
